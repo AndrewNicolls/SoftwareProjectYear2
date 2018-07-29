1ba2a957c5822b1dd29950628916310b27a9744e
Andrew <Andrew@10.0.0.3>
2018-07-29 17:41:11 +0200

    Further testing for project

--- spY2/src/spy2/SpY2.java
+++ spY2/src/spy2/SpY2.java
@@ -1,19 +1,14 @@
 package spy2;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Andrew
  */
 public class SpY2 {
 
-  
     public static void main(String[] args) {
-      Scanner sc= new Scanner(System.in);
-      
-      GUI generateGUI = new GUI();
-      generateGUI.executeGUI();
+        GUI generateGUI = new GUI();
+        generateGUI.executeGUI();
     }
-    
+
 }
